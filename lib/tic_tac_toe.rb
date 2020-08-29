d923a4377b5933eab0a62dea178fc4bb20fd68fa
energetic-certificate-0804 <mjbugay@yahoo.com>
2020-08-29 03:31:43 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -145,15 +145,14 @@ end
 
 def play(board)
   while !over?(board)
-    puts 
     turn(board)
+  end  
   if won?(board)
     puts "Congratulations " + winner(board) + "!"
   elsif draw?(board)
     puts "Cat's Game!"
   else
-  end
  end
-end 
+end
 
  
